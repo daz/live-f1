71a0e43572b62a62f7717e33107adc925045f65e
Scott James Remnant <scott@netsplit.com>
2007-03-15 18:17:54 +0000

    * configure.ac: Bump version to 0.2.7. * NEWS: Update.

--- src/http.c
+++ src/http.c
@@ -2,7 +2,7 @@
  *
  * http.c - handle web-site authentication and keyframe grabbing
  *
- * Copyright © 2006 Scott James Remnant <scott@netsplit.com>.
+ * Copyright © 2007 Scott James Remnant <scott@netsplit.com>.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
